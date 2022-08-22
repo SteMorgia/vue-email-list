ba2e8e0b3a73cd167b1ef40e4f484ab6493abf98
Stefano Morgia <ste.morgia@gmail.com>
2022-08-22 16:22:07 +0200

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ var app = new Vue ({
         emails: []
     },
     methods: {
-        getMailCasuale() {
+        
+        getMailCasuali() {
+            for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
                 this.emails.push (response.data.response);
             })
         }
+    }
     }
 })
